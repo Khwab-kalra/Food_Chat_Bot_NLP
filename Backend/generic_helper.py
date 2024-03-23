613f67323435a9c3887d4d4c4f41a3d1cab5c931
KHWAB KALRA <58007063+Khwab-kalra@users.noreply.github.com>
2024-03-23 18:06:45 +0530

    Add files via upload

---
+++ Backend/generic_helper.py
@@ -0,0 +1,11 @@
+import re
+
+def get_str_from_food_dict(food_dict: dict):
+    result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+    return result
+
+def extract_session_id(session_str : str):
+    match = re.search(r"/session/(.*?)/contexts/", session_str)
+    if match:
+            return match.group(1)
+    return ""
